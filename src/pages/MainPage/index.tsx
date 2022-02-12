422fae88bf12b9a054f864a8aed256431f1da180
DESKTOP-7DGGFV2\Dovran <sony.energizer@gmail.com>
2022-02-12 19:06:22 +0500

    Added MainPage to the pages

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,13 @@
+import Hero from 'components/Hero';
+import Galery from 'components/Galery';
+
+function MainPage(): JSX.Element {
+  return (
+    <>
+      <Hero/>
+      <Galery/>
+    </>
+  );
+}
+
+export default MainPage;
