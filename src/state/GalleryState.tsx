c0842e2bc6035faa9da06a205f2052a5294fd8dc
DESKTOP-7DGGFV2\Dovran <sony.energizer@gmail.com>
2022-02-13 21:25:28 +0500

    GaleryState contains types, reducer and context itself

--- src/state/GalleryState.tsx
+++ src/state/GalleryState.tsx
@@ -1,7 +1,13 @@
 import { Dispatch, Reducer } from 'react';
 import React from 'react';
+import {
+  galleryDeleteHandler,
+  galleryNavigateHandler, galleryResizeHandler, gallerySetFilterHandler,
+  gallerySetListHandler,
+  gallerySetLoadingHandler
+} from 'state/services/GalleryReducerCaseHandler';
 
-interface Card {
+export interface Card {
   id: number;
   albumId: number;
   title: string;
@@ -9,157 +15,76 @@ interface Card {
   url: string;
 }
 
-interface GalleryState {
-  page: number;
-  pages: Card[][];
-  size: number;
-  filterBy: number | null;
-}
-export enum GalleryActionType {
-  DELETE = '_DELETE_',
-  FILTER = '_FILTER_',
-  RESIZE = '_RESIZE_',
-  SET = '_SET_',
-  NAVIGATE = '_NAVIGATE'
+export interface FilterOption {
+  value: number;
+  label: string;
 }
 
-interface GalleryAction {
+export interface GalleryState {
+  loading: boolean;
+  page: number;
+  pages: Card[][];
+  list: Card[];
+  filterOptions: FilterOption[];
+  size: number;
+  filterValue: number | null;
+}
+
+export enum GalleryActionType {
+  SET_LOADING = '_SET_LOADING_',
+  NAVIGATE = '_NAVIGATE',
+  DELETE = '_DELETE_',
+  SET_FILTER = '_FILTER_',
+  RESIZE = '_RESIZE_',
+  SET_LIST = '_SET_'
+}
+
+export interface GalleryAction {
   type: GalleryActionType;
   payload: Partial<GalleryState>;
 }
 
-interface GalleryStateContext {
+export interface GalleryStateContextValue {
   state: GalleryState;
   dispatcher: Dispatch<GalleryAction>;
 }
 
-interface StatePersistor<TState> {
-  get: () => TState;
-  set: (state: TState) => boolean;
-  remove: () => boolean;
-}
-
-export class GalleryStatePersistor implements StatePersistor<GalleryState> {
-  private readonly galleryStateKey = 'GalleryStateKey';
-  private static $instance: GalleryStatePersistor;
-
-  private constructor(){}
-
-  static get Instance(): GalleryStatePersistor {
-    return GalleryStatePersistor.$instance || (
-      GalleryStatePersistor.$instance = new GalleryStatePersistor()
-    );
-  }
-
-  get(): GalleryState {
-    const state = localStorage.getItem(this.galleryStateKey);
-    if(state !== null) {
-      try {
-        return JSON.parse(state);
-      } catch (error) {
-        return EMPTY_GALLERY_STATE;
-      }
-    }
-    return EMPTY_GALLERY_STATE;
-  }
-
-  remove(): boolean {
-    try {
-      localStorage.removeItem(this.galleryStateKey);
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
-
-  set(state: GalleryState): boolean {
-    try {
-      localStorage.setItem(
-        this.galleryStateKey,
-        JSON.stringify(state)
-      );
-
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
-}
-
-
-const getCards = async (): Promise<Card[]> => {
-  const url = 'http://jsonplaceholder.typicode.com/photos';
-
-  try {
-    const response = await fetch(url);
-    if(response.ok) {
-      const cards: Card[] = await response.json();
-      return cards;
-    }
-
-    throw new Error(response.statusText);
-
-  } catch (error) {
-    console.error('Error while trying fetching GalleryState', error);
-    return Promise.resolve([]);
-  }
-}
-
-
-
-export const EMPTY_GALLERY_STATE: GalleryState = {
+export const INITIAL_GALLERY_STATE: GalleryState = {
+  loading: false,
   page: 1,
   pages: [],
-  size: 0,
-  filterBy: null
+  list: [],
+  filterOptions: [{value: 0, label: 'Not selected'}],
+  size: 10,
+  filterValue: null
 }
-
 
 export const galleryReducer: Reducer<GalleryState, GalleryAction> = (state, action) => {
   switch (action.type) {
-    case GalleryActionType.RESIZE: {
-      return state;
-    }
-    case GalleryActionType.FILTER: {
-      return state;
-    }
-    case GalleryActionType.DELETE: {
-      return state;
-    }
-    case GalleryActionType.SET: {
-      return state;
+    case GalleryActionType.SET_LOADING: {
+      return gallerySetLoadingHandler(state, action.payload);
     }
     case GalleryActionType.NAVIGATE: {
-      return state;
+      return galleryNavigateHandler(state, action.payload);
+    }
+    case GalleryActionType.SET_LIST: {
+      return gallerySetListHandler(state, action.payload);
+    }
+    case GalleryActionType.DELETE: {
+      return galleryDeleteHandler(state, action.payload);
+    }
+    case GalleryActionType.RESIZE: {
+      return galleryResizeHandler(state, action.payload);
+    }
+    case GalleryActionType.SET_FILTER: {
+      return gallerySetFilterHandler(state, action.payload);
     }
     default: {
       throw new Error('Unknown action type');
     }
   }
-} 
-
-
-
-export const GalleryStateContext = React.createContext<GalleryStateContext|null>(null);
-export const useGalleryState = (): GalleryState => {
-  const contextValue = React.useContext(GalleryStateContext);
-
-  if(contextValue !== null) {
-    return contextValue.state;
-  }
-
-  throw new Error('GalleryStateContext could not be null an moment of using');
-}
-
-export const useGalleryDispatcher = (): Dispatch<GalleryAction> => {
-  const contextValue = React.useContext(GalleryStateContext);
-
-  if(contextValue !== null) {
-    return contextValue.dispatcher;
-  }
-
-  throw new Error('GalleryStateContext could not be null an moment of using');
 }
 
 
+export const GalleryStateContext = React.createContext<GalleryStateContextValue|null>(null);
 
