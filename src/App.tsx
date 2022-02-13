33b482c64c1d5ebd650739d346e1f9d90383dab8
DESKTOP-7DGGFV2\Dovran <sony.energizer@gmail.com>
2022-02-13 21:32:28 +0500

    All compoents composed together into main page then into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import ThemeProvider from 'providers/ThemeProvider';
-import GalleryStateProvider from 'providers/GalleryStateProvider';
 import Routes from 'Routes';
 
 function App(): JSX.Element {
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import GalleryHero from 'pages/MainPage/components/GalleryHero';
-import GaleryContent from 'pages/MainPage/components/GaleryContent';
+import GalleryContent from 'pages/MainPage/components/GalleryContent';
 import GalleryStateProvider from 'providers/GalleryStateProvider';
 
 function MainPage(): JSX.Element {
   return (
     <GalleryStateProvider>
       <GalleryHero/>
-      <GaleryContent/>
+      <GalleryContent/>
     </GalleryStateProvider>
   );
 }
